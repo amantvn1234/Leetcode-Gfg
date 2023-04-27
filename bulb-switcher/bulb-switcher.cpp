1358ddc275e05b13d24f759655f35fce28a8c81e
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-04-28 02:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (94.38%) - LeetSync

--- bulb-switcher/bulb-switcher.cpp
+++ bulb-switcher/bulb-switcher.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int bulbSwitch(int n) {
         int i=0;
-        while(i*i<=n)
-        i++;
-        return i-1; 
+        // while(i*i<=n) // perfect square has odd flips so it became on
+        // i++;
+        //return i-1;
+        return sqrt(n);
+         
     }
 };
