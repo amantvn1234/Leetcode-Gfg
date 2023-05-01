d13a49ce14a0faf46727bea05b6fd598c47f6851
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-01 23:28:52 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (74.56%) - LeetSync

--- the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-       set<int> factors;
-       for(int i=1;i*i<=n;i++){
-           if(n%i==0){
-               factors.insert(i);
-               factors.insert(n/i);
-           }
-       }
-    if(factors.size()<k)
-    return -1;
-    auto it=factors.begin();
-    while(--k>=1){
-     it++;
-    }
-    return *it;
+        int i=1;
+        while(i*i<=n){
+            if(n%i==0)
+            {
+                k--;
+            }
+            if(k==0)
+            return i;
+            i++;
+        }
+        i--;
+        if(i*i==n) i--; // to remove duplicate factor sqrt(n)
+        while(i>=1){
+            if(n%i==0){
+                k--;
+            }
+            if(k==0)
+            return n/i;
+            i--;
+        }
+        return -1;
     }
 };
