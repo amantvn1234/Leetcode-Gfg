da16a2360ca24eb820a0deac7e24c47fca50bd6a
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-02 02:50:10 +0530

    Time: 62 ms (91.71%) | Memory: 27.5 MB (56.76%) - LeetSync

--- sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
+++ sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-class Local {
-    public:
-    Local(int k) { this->k = k; }
-    bool operator ()(vector<int>&i, vector<int>& j) {
-        return i[k]>j[k];
-     }
+// class Local {
+//     public:
+//     Local(int k) { this->k = k; }
+//     bool operator ()(vector<int>&i, vector<int>& j) {
+//         return i[k]>j[k];
+//      }
 
-   int k;
-};
+//    int k;
+// };
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        sort(score.begin(),score.end(),Local(k));
+        // sort(score.begin(),score.end(),Local(k));
+        sort(score.begin(),score.end(),[&](vector<int>&a ,vector<int>& b){return a[k]>b[k];});
         return score;
     }
 };
