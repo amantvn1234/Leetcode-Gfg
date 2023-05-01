9866363dbd4aa8524135104d09487a57b9fd5a0d
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-02 02:44:34 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (93.25%) - LeetSync

--- count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.cpp
+++ count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-int digitsum(int n){
-    int sum=0;
-    while(n){
-        sum+=(n%10);
-        n/=10;
-    }
-    return sum;
-}
+// int digitsum(int n){
+//     int sum=0;
+//     while(n){
+//         sum+=(n%10);
+//         n/=10;
+//     }
+//     return sum;
+// }
 
     int countEven(int num) {
         int ans=0;
-        for(int i=1;i<=num;i++){
-           int t=digitsum(i);
-           if(t%2==0)
-           ans++;
-        }
-        return ans;
+        // for(int i=1;i<=num;i++){
+        //    int t=digitsum(i);
+        //    if(t%2==0)
+        //    ans++;
+        // }
+        int n=num;
+        while(n){
+        ans+=(n%10);
+        n/=10;
+    }
+         return ans%2==0? num/2: (num-1)/2;
+         
     }
 };
