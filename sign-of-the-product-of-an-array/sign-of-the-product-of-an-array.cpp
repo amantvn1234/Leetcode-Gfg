80b7753314feeb88d2d3a79b1573f0560374142b
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-03 00:03:01 +0530

    Time: 4 ms (69.47%) | Memory: 10.2 MB (86.28%) - LeetSync

--- sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        bool zero=false;
         int neg=0;
         for(int i=0;i<nums.size();i++)
         {if(nums[i]==0)
-        zero=true;
+        return 0;
         else if(nums[i]<0)
         neg++;
         }
-       if(zero)
-       return 0;
-       return neg%2 ?-1:1;
+     return neg%2 ?-1:1;
     }
 };
