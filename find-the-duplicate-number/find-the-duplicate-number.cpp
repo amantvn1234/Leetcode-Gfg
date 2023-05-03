a823ae0f5bb437146d51a1f6bd827b2feb0af5d1
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-04 00:15:39 +0530

    Time: 104 ms (89.75%) | Memory: 61.1 MB (92.98%) - LeetSync

--- find-the-duplicate-number/find-the-duplicate-number.cpp
+++ find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+int storeatindex(vector<int> & nums,int cur){
+    if(nums[cur]==cur)
+    return cur;
+    swap(cur,nums[cur]);
+    
+    return storeatindex(nums,cur);
+}
     int findDuplicate(vector<int>& nums) {
     //     unordered_set<int> s;
     //     for(auto i:nums){
@@ -10,11 +17,16 @@ public:
     //    return -1; 
     // tc:O(n) sc:O(n)
 
-sort(nums.begin(),nums.end());
+// sort(nums.begin(),nums.end());
 
-for(int i=1;i<nums.size();i++)
-   if(nums[i]==nums[i-1])
-   return nums[i];
-   return -1;
+// for(int i=1;i<nums.size();i++)
+//    if(nums[i]==nums[i-1])
+//    return nums[i];
+//    return -1;
+//     }
+// tc:O(nlogn) sc:O(1)
+
+//m3:
+return storeatindex(nums,nums[0]);
     }
 };
