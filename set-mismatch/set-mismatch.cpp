76182e3414ede2135ffe1f3fb23f7379ce332742
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-03 22:38:23 +0530

    Time: 80 ms (22.09%) | Memory: 21.3 MB (88.74%) - LeetSync

--- set-mismatch/set-mismatch.cpp
+++ set-mismatch/set-mismatch.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-       vector<int> freq(nums.size()+1,0);
-       for(int i=0;i<nums.size();i++){
-           freq[nums[i]]++;
-       }
-       vector<int> ans;
-       int miss;
-       for(int i=1;i<=nums.size();i++){
-           if(freq[i]==2){ans.push_back(i);
-           }
-           else if(freq[i]==0){
-            miss=i;
-            }
-       }
-           ans.push_back(miss); 
-       return ans;
+    //    vector<int> freq(nums.size()+1,0);
+    //    for(int i=0;i<nums.size();i++){
+    //        freq[nums[i]]++;
+    //    }
+    //    vector<int> ans;
+    //    int miss;
+    //    for(int i=1;i<=nums.size();i++){
+    //        if(freq[i]==2){ans.push_back(i);
+    //        }
+    //        else if(freq[i]==0){
+    //         miss=i;
+    //         }
+    //    }
+    //        ans.push_back(miss); 
+
+
+    int miss,dupl;
+
+    for(int i=0;i<nums.size();i++){
+        if(nums[abs(nums[i])-1]<0) // if that no. is already found then us index pr -ve hoga
+         dupl=abs(nums[i]);
+         else
+        nums[abs(nums[i])-1]*=-1;   // mark -ve to value ke barabar wala index
+    }
+for(int i=0;i<nums.size();i++){
+    cout<<nums[i]<<endl;
+    if(nums[i]>0){
+       miss=i+1;
+       break;  
+    }
+   
+}
+       return {dupl,miss};
     }
 };
