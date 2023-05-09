daac61e29adc140ef2711dcc8ea7109b1335a639
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-10 02:43:34 +0530

    Time: 2 ms (41.02%) | Memory: 6.8 MB (69.57%) - LeetSync

--- spiral-matrix/spiral-matrix.cpp
+++ spiral-matrix/spiral-matrix.cpp
@@ -4,26 +4,22 @@ public:
         vector<int> ans;
         int rowst=0,colst=0,rowend=matrix.size()-1,colend=matrix[0].size()-1;
         while(rowst<=rowend&&colst<=colend){
-            int i,j;
             // print top
-            i=rowst;
-            for(j=colst;j<=colend;j++){
-                ans.push_back(matrix[i][j]);
+            for(int j=colst;j<=colend;j++){
+                ans.push_back(matrix[rowst][j]);
             }
             rowst++;
             // print left side
-            j=colend;
-            for(i=rowst;i<=rowend;i++){
-                ans.push_back(matrix[i][j]);
+            for(int i=rowst;i<=rowend;i++){
+                ans.push_back(matrix[i][colend]);
             }
             colend--;
 
             // print bottom side
             // check if we have bottom bacha hua hai if row bachega koe unvisited tabhi to print karenge
             if(rowst<=rowend){ // then row bacha hoga print karne ko
-            i=rowend;
-            for( j=colend;j>=colst;j--){
-                ans.push_back(matrix[i][j]);
+             for(int  j=colend;j>=colst;j--){
+                ans.push_back(matrix[rowend][j]);
             }
             rowend--;
             }
@@ -31,9 +27,8 @@ public:
             // print right side
             // check if we have right bacha hua hai if column bachega koe unvisited tabhi to print karenge
             if(colst<=colend){ // then column bacha hoga print karne ko
-            j=colst;
-            for(i=rowend;i>=rowst;i--){
-                ans.push_back(matrix[i][j]);
+            for(int i=rowend;i>=rowst;i--){
+                ans.push_back(matrix[i][colst]);
             }
             colst++;  // go to next colums
             }
