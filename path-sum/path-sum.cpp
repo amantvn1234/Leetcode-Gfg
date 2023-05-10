e3f6afda36626ce68d7a50f87ffb5337ba0bc839
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-11 01:24:10 +0530

    Time: 8 ms (86.04%) | Memory: 21.4 MB (42.24%) - LeetSync

--- path-sum/path-sum.cpp
+++ path-sum/path-sum.cpp
@@ -11,26 +11,12 @@
  */
 class Solution {
 public:
-bool pathsumhelper(vector<int>onepath,TreeNode*root,int target){
-     if(root==0)   // base case
-     return false;
-
-     onepath.push_back(root->val);  // onepath giving root to current node path
-
-    if(root->left==0&&root->right==0){  // base case
-        if(target==root->val)
-        return true;
-       // as we are at leaf node no need to traverse further
-    }
-
-    if(pathsumhelper(onepath,root->left,target-root->val)==true)
-    return true; // leftsubtree
-    if(pathsumhelper(onepath,root->right,target-root->val)==true)
-    return true; // rightsubtree
-    return false;
- }
     bool hasPathSum(TreeNode* root, int targetSum) {
-        vector<int> onepath;
-        return pathsumhelper(onepath,root,targetSum);
+        if(root==0)
+        return false;
+        if(root->left==0&&root->right==0){ // leaf node
+            return targetSum==root->val;
+        }
+        return hasPathSum(root->left,targetSum-root->val)||hasPathSum(root->right,targetSum-root->val);
     }
 };
