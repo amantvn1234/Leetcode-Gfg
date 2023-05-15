0429179584d67dedd94a3771e2e9947067d100fb
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-16 00:54:16 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (7.81%) - LeetSync

---
+++ count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans=0;
+        while(n>1){
+            
+            ans+=n/2;
+            n=ceil(n/2.0);
+           }
+        return ans;
+    }
+};
