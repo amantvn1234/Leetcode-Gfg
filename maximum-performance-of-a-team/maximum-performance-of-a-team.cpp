d8a27c3640158beafba1cbe70ab198c6167945f8
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-05-24 17:29:50 +0530

    Time: 108 ms (25.76%) | Memory: 36.7 MB (67.80%) - LeetSync

---
+++ maximum-performance-of-a-team/maximum-performance-of-a-team.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& nums1, vector<int>& nums2, int k) {
+             priority_queue<pair<int,int>> sortbynums2;
+        priority_queue<int,vector<int>,greater<int>> k_max;
+        for(int i=0;i<nums1.size();i++){
+            sortbynums2.push({nums2[i],nums1[i]});
+        }
+        long long k1sum=0,k1=k-1;
+        long long ans=0;
+        while(k1--){
+            auto top=sortbynums2.top();
+            k1sum+=top.second;
+            k_max.push(top.second);
+            sortbynums2.pop();
+            ans=max(ans,k1sum*top.first);
+        }
+         // for kth smallest nums2[i] we get first ans and then we update it if it increases
+        long long mini=sortbynums2.top().first,ksum=k1sum+sortbynums2.top().second;
+        k_max.push(sortbynums2.top().second);
+        sortbynums2.pop();
+         ans=max(ans,mini*ksum);
+        
+        while(sortbynums2.size()){
+            auto top=sortbynums2.top();
+            ksum-=k_max.top();
+            k_max.pop();
+            ksum+=top.second;
+            k_max.push(top.second);
+            mini=top.first;
+            ans=max(ans,mini*ksum);
+            sortbynums2.pop();
+        }
+        
+        int m=1e9+7;
+     return ans%m;  
+    }
+};
