88df2a70cafa660ad769c93dc25b376e9314834c
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-06-10 17:42:12 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (46.01%) - LeetSync

--- maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
    long long sum(int i,int m,int n){
+       
        long long l=min(i,m-1),r=min(n-1-i,m-1),a=m-1,leftindexes=n-l-r-1;
        return m+(l/2.0)*(2*a-(l-1))+(r/2.0)*(2*a-(r-1))+leftindexes;
+       // here leftindexes are added as at any place we cannot have 0 as given that all indexes should have positive integer in the condition so at places where 0 is present we put 1.
    }
     int maxValue(int n, int index, int maxSum) {
         int ans,st=1,end=maxSum;
