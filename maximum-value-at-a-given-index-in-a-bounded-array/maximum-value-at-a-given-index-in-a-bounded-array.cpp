4cb68a0b00d9781ab20b35f14fc09f356cacfa82
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-06-10 17:45:07 +0530

    Time: 5 ms (21.60%) | Memory: 6 MB (46.01%) - LeetSync

--- maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ maximum-value-at-a-given-index-in-a-bounded-array/maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
    long long sum(int i,int m,int n){
        
-       long long l=min(i,m-1),r=min(n-1-i,m-1),a=m-1,leftindexes=n-l-r-1;
-       return m+(l/2.0)*(2*a-(l-1))+(r/2.0)*(2*a-(r-1))+leftindexes;
-       // here leftindexes are added as at any place we cannot have 0 as given that all indexes should have positive integer in the condition so at places where 0 is present we put 1.
+       long long l=min(i,m-1),r=min(n-1-i,m-1),a=m-1;
+       return m+(l/2.0)*(2*a-(l-1))+(r/2.0)*(2*a-(r-1));
+       
    }
     int maxValue(int n, int index, int maxSum) {
-        int ans,st=1,end=maxSum;
+        maxSum-=n; // this is bcoz at all places no. should be positive so kam se kam 1 to hoga hi aage ka value pata karenge using binary search 
+        int ans,st=0,end=maxSum;
 
         while(st<=end){
             int mid=(end-st)/2+st;
@@ -19,6 +20,6 @@ public:
                 end=mid-1;
             }
         }
-     return ans;
+     return ans+1; // +1 bcoz us index pr 1 pahle se hi pada tha
     }
 };
