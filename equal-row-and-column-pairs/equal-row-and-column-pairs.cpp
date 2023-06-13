6fc073deea74a72e8f09803c0a899bef298f0f6e
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-06-13 17:00:58 +0530

    Time: 1122 ms (5.02%) | Memory: 569.1 MB (5.02%) - LeetSync

--- equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
+        unordered_map<string,int> columns;
         int n=grid.size(),matchcount=0;
-        for(int row=0;row<n;row++){
-            for(int col=0;col<n;col++){
-                bool match=true;
-                for(int k=0;k<n;k++){
-                    if(grid[row][k]!=grid[k][col]){
-                        match=false;
-                        break;
-                    }
-                }
-                if(match==true)
-                 matchcount++;
+        for(int col=0;col<n;col++){
+            string s="";
+            for(int row=0;row<n;row++){
+                s=s+'_'+to_string(grid[row][col]);
             }
+            columns[s]++;
+        }
+
+        for(int row=0;row<n;row++){
+            string s="";
+            for(int col=0;col<n;col++){
+                s=s+'_'+to_string(grid[row][col]);
+            }
+           if(columns[s]>0)
+           matchcount+=columns[s];
         }
         return matchcount;
     }
