7cead95ee10db3f86a366731ecef0b8b971b313f
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-07-13 17:42:39 +0530

    Time: 2 ms (43.74%) | Memory: 6.4 MB (49.00%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    vector<vector<int>> dp; 
-    int up(int i,int j,int m,int n){
-        if(i==m-1&&j==n-1)
-        return 1;
-        if(i>=m||j>=n)
-        return 0;
-
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        
-        return dp[i][j]=up(i+1,j,m,n)+up(i,j+1,m,n);
-    }
     int uniquePaths(int m, int n) {
-        dp.resize(m,vector<int>(n,-1));
-        return up(0,0,m,n);
+        vector<vector<int>> dp(m,vector<int>(n,1)); // Tabulation (iterative dp)
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+        //tc: O(m*n) as we two loops   sc:O(m*n) for table
     }
 };
