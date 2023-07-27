3b83e4bfb61357ee2c4a9cf963e601173e56661a
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-07-27 20:24:54 +0530

    Time: 4 ms (73.65%) | Memory: 6.5 MB (86.21%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-    vector<int> partitionLabels(string s) {
-        unordered_map<char,int> lastidx;
-        vector<int> ans;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            lastidx[s[i]]=i;
-        }
-
-        int i=0;
-
-        while(i<n){
-            int count=0,Lidx=lastidx[s[i]];
-
-            while(i<=Lidx){
-                count++;
-                Lidx=max(Lidx,lastidx[s[i]]);
-                i++;
-            }
-            ans.push_back(count);
-
-        }
-
-
-        // TC: O(2*n)   SC:O(26)
-        return ans;
+   vector<int> partitionLabels(string s) {
+    vector<pair<int,int>>index(26,{INT_MAX,INT_MIN});
+    int n=s.size();
+    for(int i=0;i<n;i++)
+    {
+        index[s[i]-'a'].first=min(index[s[i]-'a'].first,i);
+        index[s[i]-'a'].second=max(index[s[i]-'a'].second,i);            
     }
+    
+    vector<pair<int,int>>mergeInterval;
+    for(int i=0;i<26;i++){
+        if(index[i].first!=INT_MAX ){
+            mergeInterval.push_back(index[i]);
+        }
+    }
+    
+    sort(mergeInterval.begin(),mergeInterval.end());
+    //Now Time to merge
+    vector<int>ans;
+    
+    int start=mergeInterval[0].first, end=mergeInterval[0].second;
+    
+    for(int i=1;i<mergeInterval.size();i++){
+        
+        //If it lies in the current part
+        if(mergeInterval[i].first<=end){
+            start=min(start,mergeInterval[i].first);
+            end=max(end,mergeInterval[i].second);
+        }
+        else {
+            ans.push_back(end-start+1);
+            start=mergeInterval[i].first;
+            end=mergeInterval[i].second;
+        }
+    }
+    ans.push_back(end-start+1);
+    return ans;    
+}
+// Tc:O(n)+O(26log(26))   SC: O(26)
 };
