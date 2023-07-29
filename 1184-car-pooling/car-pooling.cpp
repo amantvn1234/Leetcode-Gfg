c68a33c918d1da35a0045c1771ae36b212b6c356
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-07-30 00:02:47 +0530

    Time: 15 ms (72.42%) | Memory: 10.7 MB (41.19%) - LeetSync

--- 1184-car-pooling/car-pooling.cpp
+++ 1184-car-pooling/car-pooling.cpp
@@ -1,44 +1,20 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int pcount=0,i=0,j=0,n;
-       map<int,int> c,l;
-       vector<pair<int,int>> coming,leaving;
-    
+        map<int,int> roadpoints;
         for(int i=0;i<trips.size();i++){
-            c[trips[i][1]]+=trips[i][0];
-            l[trips[i][2]]+=trips[i][0];
-
-        }
-        for(auto i:c){
-                coming.push_back({i.first,i.second});
-        }
-        for(auto i:l){
-            leaving.push_back({i.first,i.second});
+            roadpoints[trips[i][1]]+=trips[i][0];
+            roadpoints[trips[i][2]]-=trips[i][0];
         }
 
-        n=coming.size();
-        while(i<n){                        // i is pointing to incoming train and j is pointing to last passenger who is present on station
-            if(coming[i].first<leaving[j].first){
-                pcount+=coming[i].second;
-                i++;
-                if(pcount>capacity)
-                return false;
-            }
-            else if(coming[i].first==leaving[j].first){
-                pcount+=coming[i].second;
-                pcount-=leaving[j].second;
-                i++,j++;
-                if(pcount>capacity)
-                return false;
-            }
-            else{
-                pcount-=leaving[j].second;
-                j++;
-            }
+        int passcount=0;
+        for(auto i:roadpoints){
+            passcount+=i.second;
+            if(passcount>capacity)
+            return false;
         }
         return true;
 
-        //TC: O(n+nlogn)   two pointer +sorting    SC:O(n)
+        // TC: O(n)   SC:O(n)
     }
 };
