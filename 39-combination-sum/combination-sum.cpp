c04af067d501296bcc798f5aa7f3de29281ec9ea
amantvn1234 <106253361+amantvn1234@users.noreply.github.com>
2023-08-04 01:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (39.06%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
      
-     void combinations(vector<int>& candidates,int t,int i,vector<int> comb,vector<vector<int>> &ans){
+     void combinations(vector<int>& candidates,int t,int i,vector<int>& comb,vector<vector<int>> &ans){
       if(i==candidates.size()||candidates[i]>t){
           if(t==0)
              ans.push_back(comb);
@@ -14,6 +14,7 @@ public:
         comb.push_back(candidates[i]);
         t-=candidates[i];
         combinations(candidates,t,i,comb,ans);
+        comb.pop_back();
         }
         
      }
